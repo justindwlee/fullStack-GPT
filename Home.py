c3c3b41fa7261c1777800f4013de2fff07e96a7c
Justin Lee <dwlee1399@gmail.com>
2025-01-21 16:59:02 +0900

    Lecture 7.0~7.5

---
+++ Home.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="😊",
+)
+
+st.title("FullstackGPT Home")
+
+st.markdown(
+    """
+    # Hello!
+
+    Welcome to my FullstackGPT Portfolio!
+
+    Here are the apps I made:
+
+    - [DocumentGPT](/DocumentGPT)
+
+    - [PrivateGPT](/PrivateGPT)
+
+    - [QuizGPT](/QuizGPT)
+    """
+)
+
+st.write("hello!")
---
+++ pages/DocumentGPT.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import time
+
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📄"
+)
+
+st.title("DocumentGPT")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.write(message)
+    if save:
+        st.session_state["messages"].append({
+            "message": message,
+            "role": role,
+        })
+
+for message in st.session_state["messages"]:
+    send_message(
+        message["message"], 
+        message["role"], 
+        save=False
+    )
+
+message = st.chat_input("Send a message to the AI")
+
+if message:
+    send_message(message, "human")
+    time.sleep(1.2)
+    send_message(f"you said: {message}", "ai")
+
+    
